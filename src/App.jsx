856b0032a3a4207a9f88202a2b8ef38551c48dd6
RajeevKrSah <rajeevjnvkne@gmail.com>
2024-09-16 20:56:49 +0530

    Background and Card Done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Background from "./components/Background"
+import Foreground from "./components/Foreground"
+
+
+function App() {
+  return (
+    <div className='relativ w-full h-screen bg-zinc-800'>
+      <Background />
+      <Foreground />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import { FaRegFileAlt } from "react-icons/fa";
+function Card() {
+  return (
+    <div className=" relative w-60 h-72 rounded-[50px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden">
+      <FaRegFileAlt />
+
+      <p className="text-sm leading-tight mt-5 font-semibold">
+        Lorem ipsum dolor sit amet consectet
+      </p>
+      <div className="footer absolute bottom-0 bg-sky-200 w-full h-20 left-0 "></div>
+    </div>
+  );
+}
+
+export default Card;
