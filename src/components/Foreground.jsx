bf710670bbb618a4bb96c1eb671f1a679660c90c
RajeevKrSah <rajeevjnvkne@gmail.com>
2024-10-09 19:32:10 +0530

    Now cards are draggable

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,11 +1,29 @@
+import { useRef } from "react"
 import Card from "./Card"
 
  
 
 function Foreground() {
+  const ref = useRef(null)
+  const data = [
+    {
+      Desc:'This is the first card',
+      filesize:'.10mb',
+      close:true,
+      tag:{isOpen:true,tagTitle:"Download Now",tagColor:"green"}
+    },
+    {
+      Desc:'This is the second card',
+      filesize:'.6mb',
+      close:false,
+      tag:{isOpen:true,tagTitle:"Working on it",tagColor:"zinc"}
+    },
+  ]
   return (
-    <div className="fixed top-0 left-0 z-[3] w-full h-full ">
-        <Card/>
+    <div ref ={ref} className="fixed top-0 left-0 z-[3] w-full h-full flex gap-9 flex-wrap p-5 ">
+        {data.map((item,index)=>(
+          <Card data={item} reference={ref}/>
+        ))}
     </div>
   )
 }
